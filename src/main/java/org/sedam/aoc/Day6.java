6c11964070b0351840bed8c49e0565368b424627
Didier Loiseau <didierloiseau+git@gmail.com>
2024-12-06 18:42:09 +0100

    Day 6 part 2

--- src/main/java/org/sedam/aoc/Day6.java
+++ src/main/java/org/sedam/aoc/Day6.java
@@ -1,5 +1,6 @@
 package org.sedam.aoc;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Day6 extends Day {
@@ -26,8 +27,8 @@ public class Day6 extends Day {
     @Override
     public String part1(List<String> input) {
         boolean[][] walls = toWallArray(input);
-        int x = input.stream().filter(s -> s.contains("^")).mapToInt(s -> s.indexOf('^')).findFirst().orElseThrow();
-        int y = (int) input.stream().takeWhile(s -> !s.contains("^")).count();
+        int x = getX(input);
+        int y = getY(input);
         var dir = Direction.N;
 
         boolean[][] visitedPos = new boolean[walls.length][walls[0].length];
@@ -72,6 +73,53 @@ public class Day6 extends Day {
 
     @Override
     public String part2(List<String> input) {
-        return "";
+        boolean[][] walls = toWallArray(input);
+        int startX = getX(input);
+        int startY = getY(input);
+        var count = 0;
+
+        for (int x = 0; x < walls[0].length; x++) {
+            for (int y = 0; y < walls.length; y++) {
+                if ((x != startX || y != startY) && !walls[y][x]) {
+                    var newWalls = Arrays.stream(walls).map(w -> Arrays.copyOf(w, w.length)).toArray(boolean[][]::new);
+                    newWalls[y][x] = true;
+                    if (doesLoop(newWalls, startX, startY)) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return "" + count;
+    }
+
+    private boolean doesLoop(boolean[][] walls, int x, int y) {
+        var dir = Direction.N;
+
+        boolean[][][] visitedPos = new boolean[walls.length][walls[0].length][4];
+
+        while (isValid(x, y, walls)) {
+            if (visitedPos[y][x][dir.ordinal()]) {
+                return true;
+            } else {
+                visitedPos[y][x][dir.ordinal()] = true;
+            }
+            int nextX = x + dir.x, nextY = y + dir.y;
+            if (isValid(nextX, nextY, walls) && walls[nextY][nextX]) {
+                dir = dir.next();
+            } else {
+                x = nextX;
+                y = nextY;
+            }
+        }
+        return false;
+    }
+
+    private int getY(List<String> input) {
+        return (int) input.stream().takeWhile(s -> !s.contains("^")).count();
+    }
+
+    private int getX(List<String> input) {
+        return input.stream().filter(s -> s.contains("^")).mapToInt(s -> s.indexOf('^')).findFirst().orElseThrow();
     }
 }
