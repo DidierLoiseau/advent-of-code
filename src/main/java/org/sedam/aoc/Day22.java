3f31458d05dda8e92c5fb51e4d0ce81804a58b2a
DidierLoiseau <didierloiseau+github@gmail.com>
2024-12-22 22:31:54 +0100

    Day 22 part 2

--- src/main/java/org/sedam/aoc/Day22.java
+++ src/main/java/org/sedam/aoc/Day22.java
@@ -1,13 +1,19 @@
 package org.sedam.aoc;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.LongStream;
+
+import static java.lang.Long.parseLong;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Gatherers.windowSliding;
 
 public class Day22 extends Day {
     @Override
     public long part1Long(List<String> input) {
         return input.stream()
                 .mapToLong(s -> {
-                    long num = Long.parseLong(s);
+                    long num = parseLong(s);
                     for (int i = 0; i < 2000; i++) {
                         num ^= num * 64;
                         num %= 16777216;
@@ -19,5 +25,43 @@ public class Day22 extends Day {
                     return num;
                 })
                 .sum();
+    }
+
+    @Override
+    public long part2Long(List<String> input) {
+        return input.stream()
+                .flatMap(s ->
+                        LongStream.iterate(parseLong(s), num -> {
+                                    num ^= num * 64;
+                                    num %= 16777216;
+                                    num ^= num / 32;
+                                    num %= 16777216; // useless?
+                                    num ^= num * 2048;
+                                    num %= 16777216;
+                                    return num;
+                                })
+                                .limit(2000)
+                                .map(num -> num % 10)
+                                .boxed()
+                                .gather(windowSliding(5))
+                                .collect(toMap(
+                                        fivePrices -> fivePrices.stream()
+                                                .gather(windowSliding(2))
+                                                .map(twoPrices -> twoPrices.get(1) - twoPrices.getFirst())
+                                                .toList(),
+                                        fivePrices -> fivePrices.get(4),
+                                        (a, _) -> a))
+                                .entrySet().stream()
+                )
+                .collect(toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        Long::sum
+                ))
+                .values()
+                .stream()
+                .mapToLong(l -> l)
+                .max()
+                .orElseThrow();
     }
 }
